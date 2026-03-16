8b590849a16d6adcec3f1bf3d2965b7f16d09fae
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-03-16 20:53:05 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}" // Atrapa cualquier archivo suelto en la raíz
   ],
   theme: {
     extend: {
-      // Aquí agregamos nuestra gama de colores personalizados
       colors: {
         rosa: {
-          50: '#fdf2f8',  // Muy claro (ideal para fondos suaves)
-          100: '#fce7f3', // Claro (ideal para hover en fondos)
+          50: '#fdf2f8',
+          100: '#fce7f3',
           200: '#fbcfe8',
-          300: '#f9a8d4', // Rosa pastel
+          300: '#f9a8d4',
           400: '#f472b6',
-          500: '#ec4899', // Rosa principal (ideal para botones o iconos)
-          600: '#db2777', // Rosa oscuro (ideal para hover en botones)
+          500: '#ec4899',
+          600: '#db2777',
           700: '#be185d',
           800: '#9d174d',
-          900: '#831843', // Muy oscuro (ideal para textos contrastantes)
+          900: '#831843',
           950: '#500724',
         }
       }
